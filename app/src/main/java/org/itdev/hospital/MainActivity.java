6a90e06064f0ecbd5ac195da81bc2180229ca1fe
David Camilo Martinez Mancipe <dcammrt34@gmail.com>
2022-02-20 19:21:38 -0500

    uwu

---
+++ app/src/main/java/org/itdev/hospital/MainActivity.java
@@ -0,0 +1,16 @@
+package org.itdev.hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "DAAAAAAVID ES MUY ASPERO, ciErto quesi wilmer ", Toast.LENGTH_SHORT).show();
+    }
+}
